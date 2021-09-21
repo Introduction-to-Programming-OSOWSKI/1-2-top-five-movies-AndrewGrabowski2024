e17e6d1fe760c798bd37a0cb25b92ec2e4bb880c
AndrewGrabowski2024 <819788@mystma.org>
2021-09-21 14:31:46 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + b + c + d + e
+
+topFiveMovies("Lego movie", "your name", "lord of the rings", "harry potter and the sourcerers stone", "shrek")
