f5a97778d22335326ae0895983037cd56a8972de
AndrewGrabowski2024 <819788@mystma.org>
2021-09-21 14:49:18 -0500

    5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies(a, b, c, d, e):
-    return (a + b + c + d + e)
-topFiveMovies("Shrek ", "Shrek 2 ", "Shrek 3 ", "Shrek 4 ", "shrek 5 ")
+    return (a +" " + b + " " + c + " " + d + " " + e)
+
+topFiveMovies("Shrek" , "Shrek 2", "Shrek 3", "Shrek 4", "shrek 5")
